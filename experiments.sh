e21e95e761f99e35c1e378ca58f113a35665ef16
Alexis Le Glaunec <alexisdu51151@gmail.com>
2023-12-10 16:57:24 -0600

    Add parameters to experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -5,19 +5,20 @@ n_cores=$(nproc --all) # Get the number of cores on the machine
 
 echo Detected "$n_cores" cores
 
-if [ $# -ne  4 ]
+if [ $# -ne  5 ]
 then
-  echo "Usage: ./experiments.sh n_iter n_enqueue_dequeue n_trials"
+  echo "Usage: ./experiments.sh n_iter n_enqueue_dequeue n_trials step"
 fi
 
 n_iter=$1
 n_enqueue_dequeue=$2
 n_trials=$3
+step=$4
 
 # Print header
 printf "#1: memory_model\tn_threads\tn_iter\tn\tdur_ms\ttot_dur_work_ms\n"
 
-for ((i = 0; i <= n_cores; i+= 2))
+for ((i = 0; i <= n_cores; i+= step))
 do
   # Do several trials
   for ((j = 1; j <= n_trials; j++))
