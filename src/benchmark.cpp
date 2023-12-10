1f74f770d3a0a7958cafe1424fe4ff49b6ff6e76
alexisleglaunec <alexisdu51151@gmail.com>
2023-12-09 21:43:33 -0600

    removed untracked files

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -10,51 +10,46 @@
 #include <thread>
 #include <vector>
 #include <chrono>
+#include <cmath>
 
 using namespace std;
 
 #include "ms_queue_strong/ms_queue_strong.h"
 #include "ms_queue_weak/ms_queue_weak.h"
 
-
-// Dummy computation
-template<int N_ITER>
-void work() {
-    int res = 0;
-    for (int i = 0; i < N_ITER; i++) {
-        res += 1;
-    }
+// Perform work for some time
+static long work(volatile long n) {
+    while (n-- > 0);
+    return n;
 }
+
 
 // Each thread performs M enqueue and N dequeue
 // Q is the type of the queue
-template<class Q, int M, int N, int N_ITER>
-void enqueue_dequeue(Q& queue) {
+template<class Q>
+void enqueue_dequeue(Q& queue, int n, int n_iter) {
     // Enqueuing
-    for (int i = 0; i < M; i++) {
-        if (N_ITER > 0) {
-            // Perform some computation
-            work<N_ITER>();
-        }
+    for (int i = 0; i < n; i++) {
+        work(n_iter);
         queue.enqueue(i);
     }
     // Dequeuing
     int val = -1;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         queue.dequeue(&val);
-        work<N_ITER>();
+        work(n_iter);
     }
 }
 
-template<class Q, int M, int N, int N_ITER, int NTHREADS>
-double bench() {
+template<class Q>
+double bench(int n, int n_iter, int n_threads) {
     Q queue;
     vector<thread> threads;
 
     // Dispatch the threads
     auto start = chrono::high_resolution_clock::now(); // Start measuring
-    for (int i = 0; i < NTHREADS; i++) {
-        thread t = thread(enqueue_dequeue<Q, M / NTHREADS, N / NTHREADS, N_ITER>, ref(queue));
+    for (int i = 0; i < n_threads; i++) {
+        thread t = thread(enqueue_dequeue<Q>, ref(queue), n, n_iter);
         threads.push_back(move(t));
     }
 
@@ -71,32 +66,45 @@ double bench() {
     return dur_ms;
 }
 
-template<int N_ITER>
-void measure_work() {
+
+double measure_work(volatile long n) {
     auto start= chrono::high_resolution_clock::now();
-    work<N_ITER>();
+    work(n);
     auto end = chrono::high_resolution_clock::now();
-    double dur_work_ms = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-    cout << "work duration = " << dur_work_ms << " ns" << endl;
+    return chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 }
 
 
-#define N_THREADS 24
 int main() {
+    // Parameters
+    int n_threads = 2;
+    int n_iter = 100;
+    int n = 100000000;
+    int n_per_thread = n / n_threads;
     cout << "Benchmarking started" << endl;
 
     // Print the number of threads
-    cout << "N_THREADS = " << N_THREADS << endl;
+    cout << "N_THREADS = " << n_threads << endl;
 
-    // Duration of the work
-    measure_work<100>();
+    // Duration of the work (average)
+    double dur_work_ns = 0;
+    for (int i = 0; i < 10; i++) {
+        dur_work_ns += measure_work(n_iter);
+    }
+    dur_work_ns /= 100;
+    cout << "work duration = " << dur_work_ns << " ns" << endl;
+
+    // Total duration due to work
+    // Each thread calls work (2 * n_per_thread)
+    double tot_dur_work_ms = 2 * n_per_thread * dur_work_ns / 1e6;
+    cout << "total work duration = " << tot_dur_work_ms << " ms" << endl;
 
     // Strong queue
-    double dur_strong_ms = bench<MSQueueStrong<int>, 10000000, 10000000, 100, N_THREADS> ();
+    double dur_strong_ms = bench<MSQueueStrong<int>>(n_per_thread, n_iter, n_threads);
     cout << "Duration for strong queue = " << dur_strong_ms << "ms" << endl;
 
     // Weak queue
-    double dur_weak_ms = bench<MSQueue<int>, 10000000, 10000000, 100, N_THREADS>();
+    double dur_weak_ms = bench<MSQueue<int>>(n_per_thread, n_iter, n_threads);
     cout << "Duration for weak queue = " << dur_weak_ms << " ms" << endl;
 
     cout << "Benchmarking ended" << endl;
